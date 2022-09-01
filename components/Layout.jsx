92a6c1eb5b318ccb13f569c2a262931e9991ab7c
Bob Stanley Agbontor <stanleyagbontor63@gmail.com>
2022-09-01 05:14:10 +0000

    Done with the layout section

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,19 +1,24 @@
 import React from 'react';
 import Head from 'next/head';
-import Navbar from './Navbar';
 
-const Layout = () => {
+import Navbar from './Navbar';
+import Footer from './Footer';
+
+const Layout = ({ children }) => {
   return (
     <div className='layout'>
-      <head>
+      <Head>
         <title>ZAHA Store</title>
-      </head>
+      </Head>
       <header>
         <Navbar />
       </header>
       <main className='main-container'>
-
+        {children}
       </main>
+      <footer>
+        <Footer />
+      </footer>
     </div>
   )
 }
