e198cdfe56bfe06bd5a930e8f0868e8b274d4b59
Bob Stanley Agbontor <stanleyagbontor63@gmail.com>
2022-09-01 03:46:33 +0000

    Hook up sanity

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,24 @@
 import React from 'react'
 
-const index = () => {
+import { Product, FooterBanner, HeroBanner } from '../components';
+
+const Home = () => {
   return (
-    <div>index</div>
+    <>
+      <HeroBanner />
+
+      <div className='products-heading'>
+        <h2>Best Selling Products</h2>
+        <p>African wears of many symbols</p>
+      </div>
+
+      <div className='products-container'>
+        {['product 1', 'product 2'].map((product) => product)} 
+      </div>
+
+      <FooterBanner />
+    </>
   )
 }
 
-export default index
+export default Home
